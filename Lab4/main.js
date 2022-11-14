ea60c11fa21552879c468804915d130f98abd195
shrodinger <doremon98tybg@gmail.com>
2022-11-15 05:26:18 +0700

    lab4.6 and 4.7

--- Lab4/main.js
+++ Lab4/main.js
@@ -29,7 +29,7 @@ const percentageOfWorld3 = (population) => (population / 7900) * 100;
 
 console.log(`Nga: ${percentageOfWorld3(220).toFixed(2) + "%"}`);
 
-console.log(`Đức: ${percentageOfWorld3(66).toFixed(2) + "%"}`);
+console.log(`Germany: ${percentageOfWorld3(66).toFixed(2) + "%"}`);
 
 console.log(`Poland: ${percentageOfWorld3(12).toFixed(2) + "%"}`);
 
@@ -56,3 +56,49 @@ const percentages = [
   percentageOfWorld1(populations[3]).toFixed(1),
 ];
 console.log(percentages);
+
+// Lab 4.6
+
+const neighbours = ["China", "Lao", "Campuchia"];
+
+neighbours.push("Utopia");
+console.log("push: ", neighbours);
+
+neighbours.pop();
+console.log("Pop: ", neighbours);
+
+neighbours.indexOf("Germany") === -1
+  ? console.log("Probably not a central European country :D")
+  : console.log("Ok");
+
+const index = neighbours.indexOf("Lào");
+neighbours[index] = "Thailand";
+console.log(neighbours);
+
+//Lab 4.7
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const checkWinner = (avgDolphins, avgKoalas) => {
+  if (avgDolphins >= 2 * avgKoalas) {
+    return `Dolphins win ${avgDolphins} vs ${avgKoalas}`;
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    return `Koalas win ${avgKoalas} vs ${avgDolphins}`;
+  } else {
+    return "No team wins...";
+  }
+};
+
+// test 1
+console.log(checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49)));
+// test 2
+console.log(checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27)));
+
+//Lab 4.7
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0, 2;
+};
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0] + bills[1] + tips[1] + bills[2] + tips[2]];
+console.log(bills, tips, totals);
